df7d997f9e95bd20f72726fbd5dd98617aa0ad41
Amadou2-ops <78507045+Amadou2-ops@users.noreply.github.com>
2021-06-30 13:48:30 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,308 @@
+
+<!--
+	Author: W3layouts
+	Author URL: http://w3layouts.com
+	License: Creative Commons Attribution 3.0 Unported
+	License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+<title>gestion des etudiants </title>
+<!-- Meta tag Keywords -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Scholarly web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--// Meta tag Keywords -->
+<!-- css files -->
+<link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+<link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+<link rel="stylesheet" href="css/swipebox.css">
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<!-- //css files -->
+<!-- online-fonts -->
+<link href="//fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+<!-- //online-fonts -->
+</head>
+<body>
+<!-- banner -->
+<div color="green">
+	<div class="agileits_w3layouts_banner_nav" >
+		<nav class="navbar navbar-default">
+			<div class="navbar-header navbar-left" >
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			<h1><a class="navbar-brand" href="index.html"><i class="" aria-hidden="true"> <img src="images/4.png"/></i></a></h1>
+
+			</div>
+			
+			<ul class="agile_forms">
+				<li><a class="active" href="form/form/form.html"><i class="fa fa-sign-in" aria-hidden="true"></i> S'inscrire</a> </li>
+			<!--	<li><a href="#" data-toggle="modal" data-target="#myModal3"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Sign Up</a> </li> -->
+			</ul>
+			<ul class="agile_forms">
+				<li><a class="active" href="Students/login.html"><i class="fa fa-sign-in" aria-hidden="true"></i> connexion</a> </li>
+			<!--	<li><a href="#" data-toggle="modal" data-target="#myModal3"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Sign Up</a> </li> -->
+			</ul>
+			
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+				<nav class="link-effect-2" id="link-effect-2">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.html" class="effect-3" >Acceuil</a></li>
+						<li><a href="#about" class="effect-3 scroll">Avantages</a></li>
+						<li><a href="#services" class="effect-3 scroll">Services</a></li>
+						<li><a href="#mail" class="effect-3 scroll">Contact</a></li>
+					</ul>
+				</nav>
+
+			</div>
+		</nav>	
+		<div class="clearfix"> </div> 
+	</div>
+</div>
+<!-- banner -->
+<div class="about-bottom">
+	<div class="col-md-6 ">
+		<div class="images">
+			<div id="image"> <img src="images/.jpg" alt="" class="img-responsive" /> </div>
+		</div>
+	</div>
+	
+</div>
+
+	
+<!-- Modal2 -->
+<div class="modal fade" id="myModal3" tabindex="-1" role="dialog">
+	<div class="modal-dialog">
+	<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+			
+			</div>
+		</div>
+	</div>
+</div>
+<div class="clearfix"> </div> 
+<!-- //Modal2 -->	
+<!-- about -->
+<div class="about-top" id="about">
+	<div class="container">
+		<h3 class="w3l-title">Avantages</h3>
+		<div class="w3layouts_header">
+			<p><i class="fa fa-graduation-cap" aria-hidden="true"></i></p>
+		</div>
+		<div class="col-md-7 wthree-services-bottom-grids ">
+			<div class="wthree-services-left">
+				<img src="images/3.png" alt="">
+			</div>
+			
+			<div class="col-md-5 wthree-about-grids">
+			
+				<a href="#" class="trend-w3l" data-toggle="modal" data-target="#myModal"><span>Plus d'info</span></a>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		
+	</div>
+</div>
+<!-- modal -->
+<div class="modal about-modal w3-agileits fade" id="myModal" tabindex="-1" role="dialog">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+			</div> 
+			<div class="modal-body">
+				<img src="images/2.jpg" alt=""> 
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rutrum iaculis enim, non convallis felis mattis at. Donec fringilla lacus eu pretium rutrum. Cras aliquet congue ullamcorper. Etiam mattis eros eu ullamcorper volutpat. Proin ut dui a urna efficitur varius. uisque molestie cursus mi et congue consectetur adipiscing elit cras rutrum iaculis enim, Lorem ipsum dolor sit amet, non convallis felis mattis at. Maecenas sodales tortor ac ligula ultrices dictum et quis urna. Etiam pulvinar metus neque, eget porttitor massa vulputate. </p>
+			</div> 
+		</div>
+	</div>
+</div>
+<!-- //modal --> 
+<!-- //about -->
+<!--stats-->
+
+<!--//stats-->
+<!-- services -->
+<div class="services" id="services" >
+	<div class="container">  
+		<h3 class="w3l-title">Nos Services</h3>
+		<div class="w3layouts_header">
+			<p><i class="fa fa-graduation-cap" aria-hidden="true"></i></p>
+		</div>
+		<div class="services-w3ls-row">
+			<div class="col-xs-4 services-grid agileits-w3layouts">
+				<span class="fa fa-graduation-cap" aria-hidden="true"></span>
+				<h6>01</h6>
+				<h5>bourse d'etude</h5>
+				<p>Itaque earum rerum hic a sapiente delectus in auctor sapien. Itaque earum rerum hic a sapiente delectus in auctor sapien.</p>
+			</div>
+			<div class="col-xs-4 services-grid agileits-w3layouts">
+				<h6>02</h6>
+				<h5>Stage</h5>
+				<p>Itaque earum rerum hic a sapiente delectus in auctor sapien. Itaque earum rerum hic a sapiente delectus in auctor sapien.</p>
+				<span class="fa fa-user-o grid-w3l-ser" aria-hidden="true"></span>
+			</div>
+			<div class="col-xs-4 services-grid agileits-w3layouts">
+				<span class="fa fa-book" aria-hidden="true"></span>
+				<h6>03</h6>
+				<h5>enseignement</h5>
+				<p>Itaque earum rerum hic a sapiente delectus in auctor sapien. Itaque earum rerum hic a sapiente delectus in auctor sapien.</p>
+			</div> 
+			<div class="clearfix"> </div>
+		</div>  
+	</div>
+</div>
+
+<!-- contact -->
+<div id="mail" class="contact">
+	<div class="container">
+		<h3 class="w3l-title">Contact</h3>
+		<div class="w3layouts_header">
+			<p><i class="fa fa-graduation-cap" aria-hidden="true"></i></p>
+		</div>
+		<div class="agile_banner_bottom_grids">
+			<div class="col-md-4 col-xs-4 w3_agile_contact_grid">
+				<div class="agile_contact_grid_left">
+					<i class="fa fa-map-marker" aria-hidden="true"></i>
+				</div>
+				<div class="agile_contact_grid_right agilew3_contact">
+					<h4>Address</h4>
+					<p>Dakar, Point-E</p>
+					<p>Rue des Écrivains,Dakar Fann</p>
+
+				</div>
+			</div>
+			<div class="col-md-4 col-xs-4 w3_agile_contact_grid">
+				<div class="agile_contact_grid_left agileits_w3layouts_left">
+					<i class="fa fa-mobile" aria-hidden="true"></i>
+				</div>
+				<div class="agile_contact_grid_right agileits_w3layouts_right">
+					<h4>Phone</h4>
+					<p>Tél. : (+221) 33 869 76 76<span>Tél. : (+221) 33 869 76 77</span></p>
+				</div>
+			</div>
+			<div class="col-md-4 col-xs-4 w3_agile_contact_grid">
+				<div class="agile_contact_grid_left agileits_w3layouts_left1">
+					 <i class="fa fa-envelope-o" aria-hidden="true"></i>
+				</div>
+				<div class="agile_contact_grid_right agileits_w3layouts_right1">
+					<h4>Email</h4>
+					<p><a href="mailto:info@example.com">incubateurs@ism.edu.sn</a>
+						<span><a href="mailto:info@example.com">incubateurs@ism.edu.sn</a></span></p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		
+	</div>
+</div>
+<div id="map"></div>
+<!-- footer -->
+
+<div class="w3layouts_copy_right">
+	<div class="container">
+		<p>© Copyright 2021 ISM Digital Campus - Tous droits reservés </p>
+	</div>
+</div>
+<!-- //footer -->
+
+<!-- js-scripts -->			
+<!-- js-files -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap --> 
+<!-- //js-files -->
+<!-- Baneer-js -->
+
+<!-- Map-JavaScript -->
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>        
+			<script type="text/javascript">
+				google.maps.event.addDomListener(window, 'load', init);
+				function init() {
+					var mapOptions = {
+						zoom: 11,
+						center: new google.maps.LatLng(40.6700, -73.9400),
+						styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+					};
+					var mapElement = document.getElementById('map');
+					var map = new google.maps.Map(mapElement, mapOptions);
+					var marker = new google.maps.Marker({
+						position: new google.maps.LatLng(40.6700, -73.9400),
+						map: map,
+					});
+				}
+			</script>
+		<!-- //Map-JavaScript -->
+
+<!-- smooth scrolling -->
+<script src="js/SmoothScroll.min.js"></script>
+<!-- //smooth scrolling -->
+<!-- stats -->
+<script type="text/javascript" src="js/numscroller-1.0.js"></script>
+<!-- //stats -->
+<!-- moving-top scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#home" class="scroll" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //moving-top scrolling -->
+<!-- gallery popup -->
+<script src="js/jquery.swipebox.min.js"></script> 
+<script type="text/javascript">
+jQuery(function($) {
+	$(".swipebox").swipebox();
+});
+</script>
+<!-- //gallery popup -->
+<!--/script-->
+	<script src="js/simplePlayer.js"></script>
+			<script>
+				$("document").ready(function() {
+					$("#video").simplePlayer();
+				});
+			</script>
+<!-- //Baneer-js -->
+<!-- Calendar -->
+<script src="js/jquery-ui.js"></script>
+	<script>
+	  $(function() {
+		$( "#datepicker" ).datepicker();
+	 });
+	</script>
+<!-- //Calendar -->	
+
+<!-- //js-scripts -->
+</body>
+</html>
